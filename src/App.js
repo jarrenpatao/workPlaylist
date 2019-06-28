8e63e7adb0794a28a71fe929386bcf37afa8e29b
jarrenpatao <jarrenpatao@gmail.com>
2019-06-28 12:33:54 -0700

    fixed sizing, removed warning

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './App.css';
 
 const iframeStyle = {
-  height: "100%",
   width: "100%",
+  maxWidth: "660px",
   overflow: "hidden",
-  background: "transparent",
-  frameborder: 0
+  background:"transparent"
 }
 
 function App() {
   return (
     <div className="App">
-      <iframe allow="autoplay *; encrypted-media *;" style={iframeStyle}sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/working-man/pl.u-jV89b1Wuq7kBKj"></iframe>
+      <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style={iframeStyle} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/working-man/pl.u-jV89b1Wuq7kBKj" title="Jarren's Work Playlist"></iframe>
     </div>
   );
 }
